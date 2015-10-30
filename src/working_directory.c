58e0e589b4ad5f8a9467a3241eaa6b411460efff
Jonathan MASSUCHETTI <jonathan.massuchetti@fiduceo.fr>
2015-10-30 16:20:31 +0100

    WD get branch

--- src/working_directory.c
+++ src/working_directory.c
@@ -1,6 +1,29 @@
 #ifndef HAVE_WORKING_DIRECTORY
 #define HAVE_WORKING_DIRECTORY
 
-void phpgit_working_directory_get_status(char* status[]) {
+char * void phpgit_working_directory_get_branch(git_repository *repo, int format) {
+    int error = 0;
+    const char *branch = NULL;
+    git_reference *head = NULL;
 
+    error = git_repository_head(&head, repo);
+
+    if (error == GIT_EUNBORNBRANCH || error == GIT_ENOTFOUND)
+        zend_throw_exception("Exception", "Branch not found", 404);
+    else if (!error) {
+        branch = git_reference_shorthand(head);
+    } else
+
+
+    if (format == FORMAT_LONG)
+        printf("# On branch %s\n", branch ? branch : "Not currently on any branch.");
+        zend_throw_exception("Exception", "Not currently on any branch", 0);
+    else
+        if (branch) {
+             RETURN_NEW_STR(branch);
+        } else {
+            zend_throw_exception("Exception", "HEAD (no branch)", 1);
+        }
+
+    git_reference_free(head);
 }
