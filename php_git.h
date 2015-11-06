a556c54c219111d37369c2414fb55f46eb704f79
Jonathan MASSUCHETTI <jonathan.massuchetti@fiduceo.fr>
2015-11-06 16:04:48 +0100

    Removing license stuff, irrelevant here

--- php_git.h
+++ php_git.h
@@ -1,23 +1,3 @@
-/*
-  +----------------------------------------------------------------------+
-  | PHP Version 7                                                        |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2015 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Author:                                                              |
-  +----------------------------------------------------------------------+
-*/
-
-/* $Id$ */
-
 #ifndef PHP_GIT_H
 #define PHP_GIT_H
 #define PHP_GIT_EXTNAME "git"
@@ -39,7 +19,7 @@
 void phpgit_init(void);
 void phpgit_throw_exception_on_libgit_error(int error);
 /* Association of methods with the class */
-extern zend_function_entry git_working_directory_methods[];
+
 
 
 /* Let's include class def */
@@ -47,9 +27,6 @@ extern zend_function_entry git_working_directory_methods[];
 #ifndef HAVE_GIT_CLASS_WORKING_DIRECTORY_H
 #include <classes/working_directory.h>
 #endif
-
-
-
 
 
 extern zend_module_entry git_module_entry;
